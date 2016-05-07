743a4f49e36d76065ba208980fbb8d090b97c486
lili39 <gismanli@163.com>
2016-05-07 19:56:43 +0800

    remove Log

--- src/uuap.js
+++ src/uuap.js
@@ -1,18 +1,15 @@
 var client = require('../lib/UuapClient');
-var Log = require('log');
 var fs = require('fs');
 var URL = require('url');
 var request = require('request')
 
-var log = new Log('debug');
-
 exports.Login = function (callback) {
     var self = this;
     //获取Uuap
-    if (global.debug) log.info('获取Uuap');
+    if (global.debug) console.log('获取Uuap');
     var uuapUrl = global.uuapServer;
     client.url_get(uuapUrl, function(err, res, data) {
-        if (global.debug) log.info('获取成功');
+        if (global.debug) console.log('获取成功');
         global.lt = data.match(/name="lt" value="(.+?)"/)[1];
         global.execution = data.match(/name="execution" value="(.+?)"/)[1];
         uuapPost(callback)
@@ -22,7 +19,7 @@ exports.Login = function (callback) {
 exports._Login = function (cookies, callback) {
     var self = this
     //设置Cookie
-    if (global.debug) log.info('设置Cookie');
+    if (global.debug) console.log('设置Cookie');
     client.set_cookies(cookies);
     client.get(global.dataServer, function(err, res, data){
         if (res.statusCode == '302') {
@@ -37,7 +34,7 @@ exports._Login = function (cookies, callback) {
 function uuapPost(callback) {
     var self = this;
 
-    if (global.debug) log.info('登录中...');
+    if (global.debug) console.log('登录中...');
     //Post登录
     var form = {
         username: global.username,
@@ -63,7 +60,7 @@ function uuapPost(callback) {
             var updateUrl = res.headers.location;
 
             client.url_get(updateUrl, function(err, res, data) {
-                if (global.debug) log.info('登录成功');
+                if (global.debug) console.log('登录成功');
                 fs.writeFile('./cookie.data', client.get_cookies());
                 callback()
             });
