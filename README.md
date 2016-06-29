f6734b1a21724153b2e37cfd35a0862b0ec05640
lili39 <gismanli@163.com>
2016-06-29 22:01:33 +0800

    重构😄

--- README.md
+++ README.md
@@ -1,41 +1,16 @@
-##Uuaper
-自动登录uuap
+## Uuaper
 
-##Features
+~~baidu~~ 自动登录uuap
+
+## Features
 *   自动登录(cookie)
 
-##Get Start
+## Get Start
 
 #### install
 
 ``` bash
     npm install --save uuaper
-```
-
-#### 普通使用请求数据
-
-```js
-var uuaper = require('uuaper');
-var uuap = new uuaper({
-    username: 'xxx',
-    password: 'xxx',
-    uuapServer: 'http://xxx.baidu.com/login',
-    dataServer: 'http://yyy.baidu.com/',
-    debug: false
-});
-
-// GET请求
-uuap.getData(url, function (ret, res, data) {
-    console.log(data);
-})
-
-// POST请求
-uuap.postData(url, {
-    id: 1,
-    name: 2
-}, function (ret, res, data) {
-    console.log(data);
-})
 ```
 
 #### 使用uuaper内置server
@@ -46,8 +21,7 @@ var uuap = new uuaper({
     username: 'xxx',
     password: 'xxx',
     uuapServer: 'http://xxx.baidu.com/login',
-    dataServer: 'http://yyy.baidu.com/',
-    debug: false
+    service: 'http://yyy.baidu.com/'
 });
 
 uuap.startServer({
@@ -65,32 +39,31 @@ var uuap = new uuaper({
     username: 'xxx',
     password: 'xxx',
     uuapServer: 'http://xxx.baidu.com/login',
-    dataServer: 'http://yyy.baidu.com/',
-    debug: false
+    service: 'http://yyy.baidu.com/'
 });
 
-app.use('/api', function (req, resp) {
-    var url = 'xxx' + req.url;
-    uuap.getData(url, function (ret, res, data) {
-        resp.send(data);
-    })
-});
+app.use('/api', uuap.loadData);
 ```
 
 #### 配置项
+
 1. username  (用户名)
 2. password  (密码)
-3. uuapServer (uuap认证服务器)
-4. dataServer (数据服务器)
-5. debug (是否打开调试)
+3. uuapServer (uuap认证服务器,记得带login参数)
+4. service (如果你不知道, 你可以登出你的系统，然后取`service`参数)
 
 #### 实例方法
-1. `Login(callback)` (会自动判断通过cookie还是重新登录，第一次调用即可)
-2. `getData(url, callback)` (get请求数据)
-3. `postData(url, params, callback)` (post请求)
-4. `startServer(options)` (启用内置server，无须单独处理POST/GET请求，如果自己有server就没必要调用该方法)
+
+1. `startServer(options)` (启用内置server，无须单独处理POST/GET请求，如果自己有server就没必要调用该方法)
 
 ##TODO
+
 *  ~~支持配置项~~
+*  数据mock
 *  优化结构
 *  Do more...
+
+## History
+
+- [1.0.0] 重构，使用[bird-auth](https://www.npmjs.com/package/bird-auth)包进行cookie获取，同时优化内置server
+- [0.1.7] 老版本
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 'use strict';
 
 var fs = require('fs');
+var url = require('url');
 
 var request = require('superagent');
+var express = require('express');
+var bodyParser = require('body-parser');
 var birdAuth = require('bird-auth');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 var options = {};
 
@@ -27,6 +34,7 @@ var Uuaper = module.exports = function (params) {
 };
 
 Uuaper.prototype.loadData = function (req, res) {
+    // console.log(options.server + req.baseUrl + req.url)
     request(req.method, options.server + req.baseUrl + req.url)
         .set({Cookie: options.cookie})
         .send(req.body)
@@ -35,26 +43,43 @@ Uuaper.prototype.loadData = function (req, res) {
                 getCookie(function() {
                     request(req.method, options.server + req.baseUrl + req.url)
                         .set({Cookie: options.cookie})
+                        .send(req.body)
                         .end(function(err, resp) {
-                            res.send(resp)
+                            res.send(resp.text);
                         })
                 })
             }
             else {
-                res.send(resp.text)
+                res.send(resp.text);
             }
         })
+};
+
+Uuaper.prototype.startServer = function (params) {
+
+    options.port = params.port || 1337;
+    options.staticPath = params.staticPath || __dirname;
+    options.proxyPath = params.proxyPath || [];
+
+    for (var i = 0; i < options.proxyPath.length; i++) {
+        app.use(options.proxyPath[i], this.loadData);
+    }
+
+    app.use(express.static(options.staticPath));
+
+    app.listen(options.port, function () {
+        console.log('Server listening on http://localhost:'+options.port+', Ctrl+C to stop')
+    });
 };
 
 function getCookie(cb) {
     var uuap = new birdAuth.uuap({
         username: options.username,
         password: options.password,
-        uuapServer: options.uuapServer, // CAS auth url 
-        service: options.service // service address, if you don't know this url, you can logout you system, and get `service` parameters
-    }, function(cookie) {
+        uuapServer: options.uuapServer,
+        service: options.service,
         options.cookie = cookie;
-        cb && cb()
+        cb && cb();
         fs.writeFile('./cookie.data', cookie);
     });
 };
